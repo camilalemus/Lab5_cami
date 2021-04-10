48e849e22c3a4724ec4253edd6ab235e5cd8c060
camilalemus <lem18272@uvg.edu.gt>
2021-04-09 22:13:01 -0600

    Print images

--- Lab_5_Cami/Lab_5_Cami.ino
+++ Lab_5_Cami/Lab_5_Cami.ino
@@ -6,7 +6,11 @@
 #include <SPI.h>
 #include <SD.h>
 
-//Menu a desplegar
+
+//********************************************************************
+//                         MENU
+//********************************************************************
+
 void menu(void) {
   Serial.println("");
   Serial.println("");
@@ -14,49 +18,105 @@ void menu(void) {
   Serial.println(" 0 para mostrar los archivos de la SD: ");
   Serial.println(" 1 para mostrar la imagen del muñequito explotado: ");
   Serial.println(" 2 para mostrar la imagen de la iguanita: ");
-  Serial.println(" 3 para mostrar la imagen del gatito o pikachu: ");
+  Serial.println(" 3 para mostrar la imagen del gatito tipo pikachu: ");
   Serial.println("");
   Serial.println(" ");
 }
 
 
+//********************************************************************
+//                         IMPRIMIR IMAGENES
+//********************************************************************
+
+void Explotado(void) { //Imprimir al muñequito explotado
+  myFile = SD.open("Explotado.txt");
+  if (myFile) {
+    Serial.println("Explotado.txt:");
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening Explotado.txt");
+  }
+  menu();
+}
+
+
+void Iguanita(void) { //Imprimir a la iguanita
+  myFile = SD.open("Iguanita.txt");
+  if (myFile) {
+    Serial.println("Iguanita.txt:");
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening Iguanita.txt");
+  }
+  menu();
+}
+
+
+void Piki(void) { //Imprimir al gatito tipo pikachu
+  myFile = SD.open("Piki.txt");
+  if (myFile) {
+    Serial.println("Piki.txt:");
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening Piki.txt");
+  }
+  menu();
+}
+
+//********************************************************************
+//                        SETUP
+//********************************************************************
 void setup() {
   // put your setup code here, to run once:
 
 }
 
+
+//********************************************************************
+//                         LOOP PRINCIPAL
+//********************************************************************
+
 void loop()
 {
 
   if (Serial.available() > 0) { //Bandera para ver si el buffer está habilitado
-
     seleccion = Serial.read(); //Se lee lo que el usuario escribió
-
     if (seleccion == '0') {
- 
       myFile = SD.open("/");     // Ver los archivos que estan dentro
       printDirectory(myFile, 0);
       menu();
     }
     else if (seleccion == '1') { //Imprimir imagen de muñequito explotado
       Explotado();
-
     }
     else if (seleccion == '2') { //Imprimir imagen de la iguanita
-      Iguana();
-
+      Iguanita();
     }
+   
     else if (seleccion == '3') { //Imprimir imagen del pikachu o no se que es
-
       Piki();
-
     }
-
     else {
-      
       Serial.println("Error, ingrese un número válido "); //Progra defensiva
       menu();
-
     }
   }
 }
