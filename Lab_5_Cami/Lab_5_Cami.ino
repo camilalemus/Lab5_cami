44b954673e6bfb4a4b3991ec026caa5e0025e4ea
camilalemus <lem18272@uvg.edu.gt>
2021-04-09 21:50:13 -0600

    Menu finalizado

--- Lab_5_Cami/Lab_5_Cami.ino
+++ Lab_5_Cami/Lab_5_Cami.ino
@@ -3,18 +3,20 @@
 // Carnet: 18272
 //**************************************************************************
 
+#include <SPI.h>
+#include <SD.h>
 
 //Menu a desplegar
 void menu(void) {
   Serial.println("");
   Serial.println("");
   Serial.println(" Escriba el número correspondiente a la imagen que quiere ver: ");
-  Serial.println(" 1: ");
-  Serial.println(" 2: ");
-  Serial.println(" 3: ");
-  Serial.println(" 4: ");
+  Serial.println(" 0 para mostrar los archivos de la SD: ");
+  Serial.println(" 1 para mostrar la imagen del muñequito explotado: ");
+  Serial.println(" 2 para mostrar la imagen de la iguanita: ");
+  Serial.println(" 3 para mostrar la imagen del gatito o pikachu: ");
   Serial.println("");
-  Serial.println("");
+  Serial.println(" ");
 }
 
 
@@ -23,7 +25,38 @@ void setup() {
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly: 
-  
+void loop()
+{
+
+  if (Serial.available() > 0) { //Bandera para ver si el buffer está habilitado
+
+    seleccion = Serial.read(); //Se lee lo que el usuario escribió
+
+    if (seleccion == '0') {
+ 
+      myFile = SD.open("/");     // Ver los archivos que estan dentro
+      printDirectory(myFile, 0);
+      menu();
+    }
+    else if (seleccion == '1') { //Imprimir imagen de muñequito explotado
+      Explotado();
+
+    }
+    else if (seleccion == '2') { //Imprimir imagen de la iguanita
+      Iguana();
+
+    }
+    else if (seleccion == '3') { //Imprimir imagen del pikachu o no se que es
+
+      Piki();
+
+    }
+
+    else {
+      
+      Serial.println("Error, ingrese un número válido "); //Progra defensiva
+      menu();
+
+    }
+  }
 }
