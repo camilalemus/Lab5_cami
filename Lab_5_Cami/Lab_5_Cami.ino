b5fe8b08a0111225fd0ea833ba42dfe6d3291b6e
camilalemus <lem18272@uvg.edu.gt>
2021-04-09 19:41:54 -0600

    Initial commit MENU

---
+++ Lab_5_Cami/Lab_5_Cami.ino
@@ -0,0 +1,29 @@
+//************************************************************************** 
+// Author: Camila Lemus
+// Carnet: 18272
+//**************************************************************************
+
+
+//Menu a desplegar
+void menu(void) {
+  Serial.println("");
+  Serial.println("");
+  Serial.println(" Escriba el número correspondiente a la imagen que quiere ver: ");
+  Serial.println(" 1: ");
+  Serial.println(" 2: ");
+  Serial.println(" 3: ");
+  Serial.println(" 4: ");
+  Serial.println("");
+  Serial.println("");
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
